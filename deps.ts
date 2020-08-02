06534bd5f5e031f7d0f658f3325ae10721f152ea
Daniele <lupo.develop@gmail.com>
2020-08-02 11:09:00 +0200

    UPDATE deps
    from oak 6.0.0 to 6.0.1
    no particular reason, however, it works

--- deps.ts
+++ deps.ts
@@ -6,5 +6,4 @@ export * as log from "https://deno.land/std@0.61.0/log/mod.ts";
 export {
     Application,
     Router,
-    send,
-  } from "https://deno.land/x/oak@v6.0.0/mod.ts";
+  } from "https://deno.land/x/oak@v6.0.1/mod.ts";
