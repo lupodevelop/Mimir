e12c750db23bdee1696874235fa0fd04981eb6b3
Daniele <lupo.develop@gmail.com>
2020-08-02 21:30:19 +0200

    User api

--- controllers/UserController.ts
+++ controllers/UserController.ts
@@ -6,5 +6,17 @@ export default {
     show ( ctx : any ) {
         ctx.response.body = ctx.params.id;
     },
+    async store ( ctx  : any) {
 
+        const { value } = await ctx.request.body();
+
+        ctx.response.status = 201;
+        ctx.response.body = value;
+    },
+    update ( ctx : any ){
+        
+    },
+    destroy ( ctx : any ){
+
+    },
 }
