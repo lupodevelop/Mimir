4db8ba354c25e82c97eb469be9c03c042d029bcb
Daniele <lupo.develop@gmail.com>
2020-08-04 08:59:39 +0200

    ADDED MongoDB driver

--- deps.ts
+++ deps.ts
@@ -12,3 +12,9 @@ export {
 
 //Dotenv handling for deno.
 export { config } from "https://deno.land/x/dotenv@v0.5.0/mod.ts";
+
+//MongoDB Driver
+export { 
+  init,
+  MongoClient,
+} from "https://deno.land/x/mongo@v0.9.2/mod.ts";
